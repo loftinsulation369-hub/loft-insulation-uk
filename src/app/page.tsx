76757efc064e577170d2293854bba9331603f97d
loftinsulation369-hub <loftinsulation369@gmail.com>
2026-01-23 12:26:52 +0530

    new home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,10 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center max-w-4xl mx-auto">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-6">
-             Paise dedo , website lelo
+             There is a problem with the website
             </h2>
             <p className="text-xl text-gray-600 leading-relaxed">
-              To the owner of this website , the company you had contacted to build your project hasn't paid me a penny since september 2025
-              Therefore I am removing all the content from this website.
-              Thanks !!
+              Try to troubleshoot the issue by yourself. If you are unable to fix the issue, please contact the support team.
             </p>
           </div>
         </div>
